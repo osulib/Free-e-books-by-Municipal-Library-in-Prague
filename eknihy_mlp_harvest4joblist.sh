85d4a66632015b973cd4abd5c12be78e30eea872
Matyáš Bajger <bajger@svkos.cz>
2018-11-07 08:27:43 +0100

    Update eknihy_mlp_harvest4joblist.sh

--- eknihy_mlp_harvest4joblist.sh
+++ eknihy_mlp_harvest4joblist.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 den_spousteni="08"
+cesta_k_skriptum='/exlibris/aleph/matyas/eknihy_mlp'
 
 den=`date +%d`
 datum=`date +"%Y%m%d"`
 
-if [ "$den" == "$den_spousteni" ]; then 
+if [ "$den" == "$den_spousteni" ]; then
    #dohledani posledniho updatu
-   if [ -f /exlibris/aleph/matyas/eknihy_mlp/eknihy_mlp_update.timestamp ]; then
-      echo "Posledni update probehl `cat /exlibris/aleph/matyas/eknihy_mlp/eknihy_mlp_update.timestamp`"
-      dat_akt=`cat /exlibris/aleph/matyas/eknihy_mlp/eknihy_mlp_update.timestamp`
+   if [ -f "$cesta_k_skriptum/eknihy_mlp_update.timestamp" ]; then
+      echo "Posledni update probehl `cat $cesta_k_skriptum/eknihy_mlp_update.timestamp`"
+      dat_akt=`cat $cesta_k_skriptum/eknihy_mlp_update.timestamp`
    else
       if [ -f $alephe_scratch/eknihy_mlp_update.timestamp ]; then
          echo "Posledni update probehl `cat $alephe_scratch/eknihy_mlp_update.timestamp`"
-         dat_akt=`cat /exlibris/aleph/matyas/eknihy_mlp/eknihy_mlp_update.timestamp`
+         dat_akt=`cat $alephe_scratch/eknihy_mlp_update.timestamp`
       else
          echo "Datum posledniho updatu nenalezen, pouzivam 1 mesic zpet: "`date +"%Y-%m-%d" -d '1 month ago'`
          dat_akt=`date +"%Y-%m-%d" -d '1 month ago'`
       fi
    fi
-   echo "spoustim aktualizaci /exlibris/aleph/matyas/eknihy_mlp/eknihy_mlp_harvest.csh akt $dat_akt"
-   /exlibris/aleph/matyas/eknihy_mlp/eknihy_mlp_harvest.csh akt $dat_akt
+   echo "spoustim aktualizaci $cesta_k_skriptum/eknihy_mlp_harvest.csh akt $dat_akt"
+   $cesta_k_skriptum/eknihy_mlp_harvest.csh akt $dat_akt
 else
    echo "Spousti se $den_spousteni den v mesici, dnes nic"
 fi
